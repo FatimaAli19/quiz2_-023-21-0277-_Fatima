2456eb4c7f60879629176d6eabcbb12da1fab001
FatimaAli19 <fatimaalimemon21@gmail.com>
2023-09-27 12:28:04 +0500

    Task 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,24 @@ function percentage(number,total)
         return per=(number/100)*total;
 }
 console.log(percentage(5,60));
+//task2
+const persons=[
+	{
+		id: 1,
+		personName: "Fahad",
+		personAge: "23",
+		personIncome: "2000000"
+	},
+	{
+		id: 2,
+		personName: "Zoya",
+		personAge: "23",
+		personIncome: "3000000"
+	}
+]
+function printPerson(person) {
+        console.log(`Person ${person.id} name is ${person.personName} having salary ${person.personIncome}`);
+      }
+      for (const person of persons) {
+        printPerson(person);
+      }
